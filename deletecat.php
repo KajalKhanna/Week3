94c41bf3533ea27e7274bd703d42ce0b60ec0553
KajalKhanna <kajalkhanna803@gmail.com>
2020-12-20 20:53:12 +0530

    first commit

--- deletecat.php
+++ deletecat.php
@@ -1,11 +1,8 @@
 <?php
 session_start();
 $i=$_SESSION["id"];
-
-var_dump($v3);
-$con = mysqli_connect("localhost","task","kajal@1999") or die("cann't connect to database server");
-mysqli_select_db($con,"task") or die("database not found");
-$sql="DELETE FROM category where `id`='$i'";
+require_once('./config/db1.php');
+deleteRecord('category','id');
 $res= mysqli_query($con,$sql);
 
 
