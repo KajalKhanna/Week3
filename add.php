ebff4b473b8eebddf7f1815f15631940f9ea3415
KajalKhanna <65483908+KajalKhanna@users.noreply.github.com>
2020-12-23 16:15:01 +0530

    Add files via upload

---
+++ add.php
@@ -0,0 +1,55 @@
+<?php require_once('includes/header.php'); ?>
+<?php require_once('lib/cleanOperations.php'); ?>
+<?php require_once('lib/dbOperations.php'); ?>
+
+<fieldset>
+    <legend align="center">Add New Category Details</legend>
+
+    <form action="add.php" method="post" class="login">
+
+        <table width="100%">
+            <tr>
+                <td>Name : </td>
+                <td><input type="text" name="name" value="" /></td>
+            </tr>
+            <tr>
+                <td>Description : </td>
+                <td><input type="text" name="description" value="" /></td>
+            </tr>
+            <tr>
+                <td>Status (Active/Inactive) : </td>
+                <td><input type="text" name="status" value="" /></td>
+            </tr>
+            <tr>
+                <td>Updated  : </td>
+                <td><input type="date" name="updated" value="" /></td>
+            </tr>
+            <tr>
+                <td> &nbsp;</td>
+                <td><input type="submit" name="sub" value=" Add " /></td>
+            </tr>
+            <tr>
+                <td></br></br></br></br></td>
+                <td><a href="categories.php">Go Back to Categories Page</a></td>
+            </tr>
+        </table>
+    </form>
+</fieldset>
+
+<?php
+if (isset($_POST['sub'])) 
+{
+    $name = $_POST['name'];
+    $desc = $_POST['description'];
+    $status = $_POST['status'];
+    $updated = $_POST['updated'];
+    @$sql = "INSERT INTO category(`name`,`description`,`status`,`updated`) VALUES ('$name','$description','$status','$updated')";
+    $run = mysqli_query($con, $sql);
+    if ($run == true) {
+?>
+<?php
+    } else {
+        echo ("error:" . mysqli_error($con));
+    }
+}
+?>
