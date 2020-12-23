b5057ac3f35eb6e02dc682b55a797eaea6b91cc1
KajalKhanna <65483908+KajalKhanna@users.noreply.github.com>
2020-12-23 16:14:00 +0530

    Update deletecat.php

--- deletecat.php
+++ deletecat.php
@@ -1,9 +1,10 @@
+
 <?php
 session_start();
-$i=$_SESSION["id"];
-require_once('./config/db1.php');
-deleteRecord('category','id');
+$a=$_SESSION["id"];
+echo "Please Click on the Back Button on browser tab";
+require_once('config/db1.php');
+$sql="DELETE FROM category where `id`='$a'";
 $res= mysqli_query($con,$sql);
-
-
+$val1=mysqli_affected_rows($con);
 ?>
